12119978b758c35cb5c3f6456ea793d7bb07da4f
jerukim <mikurej95@gmail.com>
2020-11-11 17:52:37 -0500

    probelm setup for 'same'

---
+++ patterns/frequency-counters/same.js
@@ -0,0 +1,9 @@
+/*
+write a function called same, which accepts two arrays.
+the function should return true if every value in the array has it's corresponding values squared in the second array.
+the frequency of values must be the same.
+*/
+
+function same() {
+
+}

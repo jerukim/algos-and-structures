a666e9da2d722bbe200655ee11dc6bf3e35a5f06
jerukim <mikurej95@gmail.com>
2020-11-11 17:54:51 -0500

    defined inputs

--- patterns/frequency-counters/same.js
+++ patterns/frequency-counters/same.js
@@ -4,6 +4,13 @@ the function should return true if every value in the array has it's correspondi
 the frequency of values must be the same.
 */
 
+/* INPUT: 2 arrays (arr1, arr2)
+arr1 and arr2 must have the same length
+do we know which array is going to be squared? 
+    we can pragmatically determine this by comparing the first element of each array to see which one is larger
+are the array elements always going to be numbers? we can type check and return false
+*/
+
 function same() {
 
 }

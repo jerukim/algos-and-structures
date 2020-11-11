04dac8228ac39a094100d698f7294afd58f4027c
jerukim <mikurej95@gmail.com>
2020-11-11 18:44:29 -0500

    anagrams problem setup

---
+++ patterns/frequency-counters/anagrams.js
@@ -0,0 +1,9 @@
+/* PROBLEM:
+given two strings, write a function to determine if the second string is an aanagram of the first.
+an anagram is a word, phrase or name formed by rearranging the letters of another
+example: 'cinema' can be formed from 'iceman'
+*/
+
+function validAnagram(a, b) {
+
+}

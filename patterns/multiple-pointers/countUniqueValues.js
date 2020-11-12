0adc8f28222af28956bc745fe26b1f64b7b6d488
jerukim <mikurej95@gmail.com>
2020-11-11 19:48:13 -0500

    problem setup for countUniqueValues

---
+++ patterns/multiple-pointers/countUniqueValues.js
@@ -0,0 +1,8 @@
+/* PROBLEM:
+Implement a function called countUniqueValues which accepts a sorted array, and counts the unqiue values in the array.
+There can be negative numbers in the array, but it will always be sorted.
+*/
+
+function countUniqueValues(array) {
+
+}

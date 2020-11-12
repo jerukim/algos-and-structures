8481149ed740e20b4c5e9f857febec2ab0c11b53
jerukim <mikurej95@gmail.com>
2020-11-11 19:12:42 -0500

    final solution for anagrams

--- patterns/frequency-counters/anagrams.js
+++ patterns/frequency-counters/anagrams.js
@@ -16,26 +16,59 @@ capital and lowercase letters > capital cases should count as lowercase
 
 function validAnagram(a, b) {
     // return false if string lenghts are not equal
+    if (a.length !== b.length) {
+        return false
+    }
 
     // create a frequency counter object
+    const frequency = {}
 
     // loop over characters of a
-    // check if that the character is alphabetical
-    // lowercase the character
-    // iterate counter in frequency object for that letter
+    for (let char of a) {
+        // check if that the character is alphabetical
+        if (isAlphabetical(char)) {
+            // lowercase the character
+            char = char.toLowerCase()
+            // iterate counter in frequency object for that letter
+            frequency[char] = (frequency[char] || 0) + 1
+        }
+    }
 
     // loop over characters of b
-    // check if that the character is alphabetical
-    // lowercase the character
-    // if character exists in frequency
-    // decrement counter in frequency object for that letter
-    // else return false
+    for (let char of b) {
+        // check if that the character is alphabetical
+        if (isAlphabetical(char)) {
+            // lowercase the character
+            char = char.toLowerCase()
+            // if character exists in frequency
+            if (frequency[char]) {
+                // decrement counter in frequency object for that letter
+                frequency[char]--
+            } else {
+                // else return false
+                return false
+            }
+
+        }
+    }
 
     // loop over values of frequency object
-    // if value is not 0, return false
+    for (const characterCount of Object.values(frequency)) {
+        // if value is not 0, return false
+        if (characterCount !== 0) {
+            return false
+        }
+    }
 
-    // return true
+    return true
+}
 
+function isAlphabetical(char) {
+    const charCode = char.charCodeAt(0)
+    const isCapitalLetter = charCode > 64 && charCode < 91
+    const isLowercaseLetter = charCode > 96 && charCode < 123
+
+    return isCapitalLetter || isLowercaseLetter
 }
 
 // TEST CASES
@@ -48,3 +81,4 @@ console.log(validAnagram('hello', 'olelh') === true ? 'Pass' : 'Fail')
 console.log(validAnagram('hello', 'olclh') === false ? 'Pass' : 'Fail')
 console.log(validAnagram('tar', 'rat') === true ? 'Pass' : 'Fail')
 console.log(validAnagram('tar', 'car') === false ? 'Pass' : 'Fail')
+console.log(validAnagram('asdfasdfwerwef', 'aspfmsdfcerwsf') === false ? 'Pass' : 'Fail')

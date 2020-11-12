8eae4d32a90935102d9ef6fdcaea721ade74d876
jerukim <mikurej95@gmail.com>
2020-11-11 22:40:34 -0500

    problem setup for binarySearch

---
+++ patterns/divide-and-conquer/binarySearch.js
@@ -0,0 +1,7 @@
+/* PROBLEM:
+Given a sorted array of integers, write a function called binarySearch, that accepts a value and returns the index where the value passed to the function is located. If the value is not found, return -1
+*/
+
+function binarySearch() {
+
+}

9b089e724e54d8dcc0a5d6665d3ca0d92742776d
jerukim <mikurej95@gmail.com>
2020-11-11 23:37:23 -0500

    iterative solution for binarySearch

--- patterns/divide-and-conquer/binarySearch.js
+++ patterns/divide-and-conquer/binarySearch.js
@@ -10,15 +10,26 @@ Given a sorted array of integers, write a function called binarySearch, that acc
 2. num that is not in array > return -1
 */
 
-function binarySearch(array, num) {
+function binarySearchIterative(array, value) {
+    let min = 0
+    let max = array.length - 1
 
+    while (max >= min) {
+        const center = Math.floor((max + min) / 2)
+
+        if (array[center] === value) return center
+        else if (array[center] < value) min = center + 1
+        else if (array[center] > value) max = center - 1
+    }
+
+    return -1
 }
 
 // TEST CASES:
-console.log(binarySearch([], 1) === -1)
-console.log(binarySearch([1], 1) === 0)
-console.log(binarySearch([1, 2, 3], 1) === 0)
-console.log(binarySearch([1, 2, 3], 2) === 1)
-console.log(binarySearch([1, 2, 3, 4, 5, 6], 4) === 3)
-console.log(binarySearch([1, 2, 3, 4, 5, 6], 6) === 5)
-console.log(binarySearch([1, 2, 3, 4, 5, 6], 11) === -1)
+console.log(binarySearchIterative([], 1) === -1)
+console.log(binarySearchIterative([1], 1) === 0)
+console.log(binarySearchIterative([1, 2, 3], 1) === 0)
+console.log(binarySearchIterative([1, 2, 3], 2) === 1)
+console.log(binarySearchIterative([1, 2, 3, 4, 5, 6], 4) === 3)
+console.log(binarySearchIterative([1, 2, 3, 4, 5, 6], 6) === 5)
+console.log(binarySearchIterative([1, 2, 3, 4, 5, 6], 11) === -1)

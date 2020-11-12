f51a7c005bd5af94ecdcfb164278d50caf385b77
jerukim <mikurej95@gmail.com>
2020-11-11 19:53:40 -0500

    define input, output, edge cases and tests for countUniqueValues

--- patterns/multiple-pointers/countUniqueValues.js
+++ patterns/multiple-pointers/countUniqueValues.js
@@ -3,6 +3,21 @@ Implement a function called countUniqueValues which accepts a sorted array, and 
 There can be negative numbers in the array, but it will always be sorted.
 */
 
+// INPUT: sorted array
+// OUTPUT: integer of unique numbers in array
+
+/* EDGE CASES:
+empty array
+*/
+
 function countUniqueValues(array) {
 
 }
+
+// TEST CASES:
+console.log(countUniqueValues([]) === 0)
+console.log(countUniqueValues([1]) === 1)
+console.log(countUniqueValues([1, 1]) === 1)
+console.log(countUniqueValues([1, 2]) === 2)
+console.log(countUniqueValues([-1, 1, 2]) === 3)
+console.log(countUniqueValues([-4, -4, -1, 0, 1, 2, 5, 5, 7]) === 7)

023120e87a9eaea8cbad0a10283c17d7c5fe6ed0
jerukim <mikurej95@gmail.com>
2020-11-12 18:54:35 -0500

    Create minSubArrayLen.js

---
+++ minSubArrayLen.js
@@ -0,0 +1,8 @@
+/* PROBLEM:
+Write a function called minSubArrayLen which accepts two parameters - an array of positive integers and a postive integer.
+This function should return the minimal length of a contiguous subarray of which the sum is greater than or equal to the integer passed to the function. If there isn't one, return 0 instead.
+*/
+
+function minSubArrayLen() {
+
+}

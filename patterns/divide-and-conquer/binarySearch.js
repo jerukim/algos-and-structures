5f1a86d2e01eb5ef1e7a54b16f9e43f2e708ca15
jerukim <mikurej95@gmail.com>
2020-11-11 22:45:27 -0500

    define input/put, edge cases and tests for binarySearch

--- patterns/divide-and-conquer/binarySearch.js
+++ patterns/divide-and-conquer/binarySearch.js
@@ -2,6 +2,23 @@
 Given a sorted array of integers, write a function called binarySearch, that accepts a value and returns the index where the value passed to the function is located. If the value is not found, return -1
 */
 
-function binarySearch() {
+// INPUT: sorted array, number to look for (array, num)
+// OUTPUT: index of num in array
+
+/* EDGE CASES:
+1. an empty array
+2. num that is not in array > return -1
+*/
+
+function binarySearch(array, num) {
 
 }
+
+// TEST CASES:
+console.log(binarySearch([], 1) === -1)
+console.log(binarySearch([1], 1) === 0)
+console.log(binarySearch([1, 2, 3], 1) === 0)
+console.log(binarySearch([1, 2, 3], 2) === 1)
+console.log(binarySearch([1, 2, 3, 4, 5, 6], 4) === 3)
+console.log(binarySearch([1, 2, 3, 4, 5, 6], 6) === 5)
+console.log(binarySearch([1, 2, 3, 4, 5, 6], 11) === -1)
